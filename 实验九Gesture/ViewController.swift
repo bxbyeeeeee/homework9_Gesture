58ea0ee8d233168deba4aef07199ef121cfc189b
student <student@imac-21.local>
2018-12-06 15:32:59 +0800

    Initial Commit

---
+++ 实验九Gesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  实验九Gesture
+//
+//  Created by student on 2018/12/6.
+//  Copyright © 2018年 baoxinbei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
